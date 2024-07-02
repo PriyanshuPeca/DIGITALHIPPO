b7121eb0d9e86615d114af94322cf7b88972eb0f
PriyanshuPeca <priyanshu22223@gmail.com>
2024-07-03 01:55:17 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link"
+import MaxwithWrapper from "./MaxWidthWrapper"
+import { Icons } from "./Icons"
+import NavItems from "./NavItems"
+
+const Navbar=()=>{
+
+return(
+
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+
+        <header className="relative bg-white"></header>
+        <MaxwithWrapper>
+            <div className='border-b border-gray-200'>
+            <div className='flex h-16 items-center'>
+               {/* TODO MOBILE NAV*/}
+
+               <div className='ml-4 flex lg:ml-0'>
+                <Link href='/'>
+                <Icons.logo className='h-10 w-10'/>
+                </Link>
+               </div>
+               <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+                       <NavItems/>       
+               </div>
+               
+            </div>
+            </div>
+        </MaxwithWrapper>
+    </div>
+
+
+)
+
+   
+}
+export default Navbar
